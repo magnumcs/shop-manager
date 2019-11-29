7ea73408e3b9f2d3c276a308238359fe83b3d08e
mfaria <mfaria@fazenda>
2019-11-29 15:56:25 -0400

    Primeiro commit shop-config-server

---
+++ shop-config-server/src/test/java/com/portifolio/magnum/shopconfigserver/ShopConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.magnum.shopconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
